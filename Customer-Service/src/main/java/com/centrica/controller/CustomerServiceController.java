59d43173ca726c848c8af693b0059a9d08f3e651
Pradee08 <66717995+Pradee08@users.noreply.github.com>
2020-06-16 11:14:53 +0530

    updated customer controller class

--- Customer-Service/src/main/java/com/centrica/controller/CustomerServiceController.java
+++ Customer-Service/src/main/java/com/centrica/controller/CustomerServiceController.java
@@ -3,33 +3,52 @@ package com.centrica.controller;
 import java.util.List;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.centrica.model.Customer;
 import com.centrica.service.CustomerService;
 
 @RestController
+@RequestMapping("/customers")
 public class CustomerServiceController {
 	@Autowired
 	private CustomerService service;
-
-	@GetMapping("/customers/{ucrn}")
-	public List<Customer> get(@PathVariable String ucrn) {
-		return service.get(ucrn);
+	@GetMapping
+	public  ResponseEntity<List<Customer>> retrieve(@RequestParam String ucrn) {
+		try{
+		List<Customer> customer=service.retrieve(ucrn);
+		if(!customer.isEmpty())
+			return new ResponseEntity<List<Customer>>(customer,HttpStatus.OK);
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		catch(Exception e){
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
 	}
-
-	@PostMapping("/customers")
-	public void add(@Valid @RequestBody Customer customer) {
+				
+	@PostMapping
+	public ResponseEntity<?> add(@Valid @RequestBody Customer customer) {
 		service.add(customer);
+		return new ResponseEntity<>(HttpStatus.CREATED);
 	}
-
-	@PutMapping("/customers/{ucrn}")
-	public void update(@RequestBody Customer customer, @PathVariable String ucrn) {
-		List<Customer> customerlist = service.get(ucrn);
-		service.update(customer, ucrn, customerlist);
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<?> update(@RequestBody Customer customer, @PathVariable int id) {
+		try{
+			Customer existCustomer= service.retrivecustomer(id);
+			service.update(customer, id, existCustomer);
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+	catch(Exception e){
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	}
 	}
 }
